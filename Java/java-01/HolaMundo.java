7fddef8ada00ffdcb3f70c07cd812c9a831e491d
HorusEye33 <ralfdrummer@live.com>
2022-08-16 18:44:17 -0500

    Introducción a Java

---
+++ Java/java-01/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola Mundo Java";
+        String nombre = "Rafael Andony";
+        System.out.println("Saludos desde Java a: " + nombre);
+    }
+}
